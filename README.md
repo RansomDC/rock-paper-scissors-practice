cdfba9425d08214209887084570c997c28b2b533
Ransom <ransomcadorette@gmail.com>
2022-01-16 19:11:01 -0800

    Updated the code to remove the 5 game counter. Added DOM elements to show in the HTML what the result of the game was. Still need to add counters to show the score between the player and the computer.Altered the code for playround() so that it no longer sends unnecessary code to playRound() (which will no longer be used).

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rock-paper-scissors-practice
 A simple program to play rock, paper, scissors against the computer.
+
+//1/15/22 Starting an updated version of the program that will have a user interface so that the user can click on buttons instead of entering answer into the console.
--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 
+//Give each button an event so that when you click it it plays a round.
+
+const resultContainer = document.querySelector('.results');
+const buttons = document.querySelectorAll('button'); 
+const resultText = document.createElement('p');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.className);
+        playRound((button.className), computerPlay());
+    });
+});
+
+
+
 
 //playround(input, computerplay()): Take user input and computer input of two plays for a round of Rock, Paper, Scissors (RPS)
 function playRound(playerPlay, computerPlay) {
-    let PPCorrected = playerPlay.toLowerCase();
     let result;
-    if ((PPCorrected === "rock") || (PPCorrected === "paper") || (PPCorrected === "scissors")) {
-        if (whoWins(PPCorrected, computerPlay) === 1) {
-            result = 1;
-            console.log(vicMess(PPCorrected));
-        }else if (whoWins(PPCorrected, computerPlay) === 0) {
-            result = 0;
-            console.log(lossMess(PPCorrected));
-        } else {
-            result = "tie";
-            console.log("You tied!");
-        }
+
+    if (getVictoryResults(playerPlay, computerPlay) === true) {
+        vicMess(playerPlay);
+
+    }else if (getVictoryResults(playerPlay, computerPlay) === false) {
+        lossMess(playerPlay);
+
     } else {
-        console.log("You must enter rock, paper, or scissors!");
+        resultText.textContent = "You tied!";
+        resultContainer.appendChild(resultText);
     }
     return result;
 }
@@ -29,76 +40,77 @@ function computerPlay() {
 }
 
 //Compare the two "plays". Return 1 if player was victorious, return 0 if computer was victorious.
-function whoWins(playerPlay, computerPlay) {
+function getVictoryResults(playerPlay, computerPlay) {
     let gameResult;
     if (playerPlay === 'rock' && computerPlay === 'paper') {
-        gameResult = 0;
+        gameResult = false;
     }else if (playerPlay === 'rock' && computerPlay === 'scissors') {
-        gameResult = 1;
+        gameResult = true;
     }else if (playerPlay === 'paper' && computerPlay === 'rock') {
-        gameResult = 1;
+        gameResult = true;
     }else if (playerPlay === 'paper' && computerPlay === 'scissors') {
-        gameResult = 0;
+        gameResult = false;
     }else if (playerPlay === 'scissors' && computerPlay === 'paper'){
-        gameResult = 1;
+        gameResult = true;
     }else if (playerPlay === 'scissors' && computerPlay === 'rock'){
-        gameResult = 0;
+        gameResult = false;
     } else {
-        gameResult = undefined;
+        gameResult = "tie";
     }
     return gameResult;
 }
 
 //vicMess() provides a victory message based on what you played 
 function vicMess(playerPlay) {
-    let message;
+    
     if (playerPlay === 'rock') {
-        message = "You won! Rock beats scissors!";
+        resultText.textContent = 'You won! Rock beats scissors!'
     } else if (playerPlay === 'paper') {
-        message = "You won! Paper beats rock!";
+        resultText.textContent =  'You won! Paper beats rock!'
     } else {
-        message = "You won! Scissors beats paper!";
+        resultText.textContent =  'You won! Scissors beats paper!'
     }
-    return message;
+    resultContainer.appendChild(resultText);
 }
 
 //lossMess() provides a loss message based on what you played
 function lossMess(playerPlay) {
+  
     if (playerPlay === 'rock') {
-        message = "You lost! Paper beats rock!";
+        resultText.textContent = 'You lost! Paper beats rock!'
     } else if (playerPlay === 'paper') {
-        message = "You lost! Scissors beats paper!";
+        resultText.textContent =  'You lost! Scissors beats paper!'
     } else {
-        message = "You lost! Rock beats scissors!";
+        resultText.textContent =  'You lost! Rock beats scissors!'
     }
-    return message;
+    resultContainer.appendChild(resultText);
 }
 
 
 //game(): initiate playround() five times, store the score of each player, and after all rounds print who won.
-function game() {
-    let compScore = 0;
-    let playerScore = 0;
-    let roundResult;
-    for (let i = 0; i <5; i++) {
-        roundResult = playRound(prompt("Please enter rock, paper, or scissors!"), computerPlay());
-        if (roundResult === 1) {
-            playerScore++;
-         } else if (roundResult === 0) {
-            compScore++;
-        } else {
-            
-    }
-
-    if ((i === 4) && (playerScore > compScore)) {
-        console.log("Great job! You won " + playerScore + " to " + compScore + "!");
-    } else if ((i === 4) && (playerScore < compScore)) {
-        console.log("Bummer, the computer beat you " + compScore + " to " + playerScore + ".");
-    } else if ((i === 4) && (playerScore === compScore)) {
-        console.log("You tied with the computer. " + playerScore + " to " + compScore + ".");
-    }
-}
-}
+//function game() {
+//    let compScore = 0;
+//    let playerScore = 0;
+//    let roundResult;
+//    for (let i = 0; i <5; i++) {
+//        roundResult = playRound(prompt("Please enter rock, paper, or scissors!"), computerPlay());
+//        if (roundResult === 1) {
+//            playerScore++;
+//         } else if (roundResult === 0) {
+//            compScore++;
+//        } else {
+//            
+//    }
+//
+//    if ((i === 4) && (playerScore > compScore)) {
+//        console.log("Great job! You won " + playerScore + " to " + compScore + "!");
+//    } else if ((i === 4) && (playerScore < compScore)) {
+//        console.log("Bummer, the computer beat you " + compScore + " to " + playerScore + ".");
+//    } else if ((i === 4) && (playerScore === compScore)) {
+//        console.log("You tied with the computer. " + playerScore + " to " + compScore + ".");
+//    }
+//}
+//}
 
     //store the number of times you win and lose
     //After all rounds print who won the round
